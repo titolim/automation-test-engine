164b8a9dcb8aa1b3fca0716e1a09ff9830b01bf2
Jacques <wpbxwpbx@gmail.com>
2017-08-22 22:14:11 -0400

    1 Create Sauce Lab testcase and local brower testcase 2 Update dependencies 3 Supporte new Firefox webdriver 4 Update new webdriver

--- org.bigtester.ate.core/src/main/java/org/bigtester/ate/model/page/atewebdriver/MyFirefoxDriver.java
+++ org.bigtester.ate.core/src/main/java/org/bigtester/ate/model/page/atewebdriver/MyFirefoxDriver.java
@@ -20,6 +20,7 @@
  *******************************************************************************/
 package org.bigtester.ate.model.page.atewebdriver;
 
+import org.bigtester.ate.GlobalUtils;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
@@ -35,13 +36,38 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  */
 public class MyFirefoxDriver extends AbstractWebDriverBase implements
 		IMyWebDriver {
+	
+	/** The GECKON Driver Name and Path. */	 
+	/** The Constant BROWSERNAME. */
+	private static final String MARIONETTENAME = "marionette";
+	/** The Constant BROWSERDRVNAME. */
+	private static final String BROWSERDRVNAME = "webdriver.gecko.driver";
+	/** The Constant BROWSERWIN32PATH. */
+	private static final String BROWSERWIN32PATH = "windows/marionette/32bit/";
+	/** The Constant BROWSERWIN64PATH. */
+	private static final String BROWSERWIN64PATH = "windows/marionette/64bit/";
+	/** The Constant BROWSERLINUX32PATH. */
+	private static final String BROWSERLINUX32PATH = "linux/marionette/32bit/";
+	/** The Constant BROWSERLINUX64PATH. */
+	private static final String BROWSERLINUX64PATH = "linux/marionette/64bit/";
+	/** The Constant BROWSEROSX32PATH. */
+	private static final String BROWSEROSX32PATH = "osx/marionette/32bit/";
+	/** The Constant BROWSEROSX32PATH. */
+	private static final String BROWSEROSX64PATH = "osx/marionette/64bit/";
+	/** The Constant BROWSERWINFILENAME. */
+	private static final String BROWSERWINFILENAME = "geckodriver.exe";
+	/** The Constant BROWSERLINUXFILENAME. */
+	private static final String BROWSERLINUXFILENAME = "geckodriver";
+	/** The Constant BROWSERMACFILENAME. */
+	private static final String BROWSER0SXFILENAME = "geckodriver";
+
 
 	/** The browser profile. */
 	@Nullable
 	final private FirefoxFeatureProfile browserProfile;
 	
 	/** The Constant BROWSERTYPENAME. */
-	final public static String BROWSERTYPENAME = "Firefox"; 
+	final public static String BROWSERTYPENAME = "firefox"; 
 	/**
 	 * Instantiates a new my firefox driver.
 	 */
@@ -80,23 +106,88 @@ public class MyFirefoxDriver extends AbstractWebDriverBase implements
 	}
 
 	/**
+	 * Sets the firefox driver system env.
+	 */
+	public static void setFirefoxDriverSystemEnv() {
+		OSinfo osinfo = new OSinfo();
+		EPlatform platform = osinfo.getOSname();
+		String driverPath = GlobalUtils.getDriverPath(); //NOPMD		
+		
+		switch (platform) {
+		case Windows_32:
+			/*versionNum = ReadXmlFile.parserXml(ReadXmlFile.REPOFILENAME, "windows", BROWSERNAME, ReadXmlFile.VERSION);*/
+			if (driverPath == null)
+				System.setProperty(BROWSERDRVNAME, GlobalUtils.DEFAULT_DRIVER_PATH + GlobalUtils.PATH_DELIMITER 
+						           + BROWSERWIN32PATH + BROWSERWINFILENAME);
+			else
+				System.setProperty(BROWSERDRVNAME, driverPath + GlobalUtils.PATH_DELIMITER 
+						           + BROWSERWIN32PATH + BROWSERWINFILENAME);
+				System.setProperty(BROWSERDRVNAME, "E:\\geckodriver.exe");
+			break;
+		case Windows_64:
+			/*versionNum = ReadXmlFile.parserXml(ReadXmlFile.REPOFILENAME, "windows", BROWSERNAME, ReadXmlFile.VERSION);*/
+			if (driverPath == null)
+				System.setProperty(BROWSERDRVNAME, GlobalUtils.DEFAULT_DRIVER_PATH + GlobalUtils.PATH_DELIMITER 
+						           + BROWSERWIN64PATH + BROWSERWINFILENAME);	
+			else
+				System.setProperty(BROWSERDRVNAME, driverPath + GlobalUtils.PATH_DELIMITER 
+						           + BROWSERWIN64PATH + BROWSERWINFILENAME);
+			break;
+		case Linux_32:
+			/*versionNum = ReadXmlFile.parserXml(ReadXmlFile.REPOFILENAME, "linux", BROWSERNAME, ReadXmlFile.VERSION);*/
+			if (driverPath == null)
+				System.setProperty(BROWSERDRVNAME, GlobalUtils.DEFAULT_DRIVER_PATH + GlobalUtils.PATH_DELIMITER 
+						           + BROWSERLINUX32PATH + BROWSERLINUXFILENAME);	
+			else
+				System.setProperty(BROWSERDRVNAME, driverPath + GlobalUtils.PATH_DELIMITER 
+						           + BROWSERLINUX32PATH + BROWSERLINUXFILENAME);
+			break;
+		case Linux_64:
+			/*versionNum = ReadXmlFile.parserXml(ReadXmlFile.REPOFILENAME, "linux", BROWSERNAME, ReadXmlFile.VERSION);*/
+			if (driverPath == null)
+				System.setProperty(BROWSERDRVNAME, GlobalUtils.DEFAULT_DRIVER_PATH + GlobalUtils.PATH_DELIMITER 
+						           + BROWSERLINUX64PATH + BROWSERLINUXFILENAME);	
+			else
+				System.setProperty(BROWSERDRVNAME, driverPath + GlobalUtils.PATH_DELIMITER 
+						           + BROWSERLINUX64PATH + BROWSERLINUXFILENAME);
+			break;
+		case Mac_OS_X_32:
+			/*versionNum = ReadXmlFile.parserXml(ReadXmlFile.REPOFILENAME, "osx", BROWSERNAME, ReadXmlFile.VERSION);*/
+			if (driverPath == null)
+				System.setProperty(BROWSERDRVNAME, GlobalUtils.DEFAULT_DRIVER_PATH + GlobalUtils.PATH_DELIMITER 
+						           + BROWSEROSX32PATH + BROWSER0SXFILENAME);
+			else 
+				System.setProperty(BROWSERDRVNAME, driverPath + GlobalUtils.PATH_DELIMITER 
+						           + BROWSEROSX32PATH + BROWSER0SXFILENAME);				
+			break;
+		case Mac_OS_X_64:
+			/*versionNum = ReadXmlFile.parserXml(ReadXmlFile.REPOFILENAME, "osx", BROWSERNAME, ReadXmlFile.VERSION);*/
+			if (driverPath == null)
+				System.setProperty(BROWSERDRVNAME, GlobalUtils.DEFAULT_DRIVER_PATH + GlobalUtils.PATH_DELIMITER 
+						           + BROWSEROSX64PATH + BROWSER0SXFILENAME);	
+			else
+				System.setProperty(BROWSERDRVNAME, driverPath + GlobalUtils.PATH_DELIMITER 
+						           + BROWSEROSX64PATH + BROWSER0SXFILENAME);
+			break;
+		default:
+			throw GlobalUtils.createNotInitializedException("operating system is not supported ");
+		}
+	}
+	
+	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public WebDriver getWebDriverInstance() {
 		WebDriver retVal = super.getWebDriver();
 		if (null == retVal) {
-			FirefoxFeatureProfile bPro = getBrowserProfile();
-			System.setProperty("webdriver.gecko.driver", "E:\\geckodriver.exe");			 
-
-			//Now you can Initialize marionette driver to launch firefox
-			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-			capabilities.setCapability("marionette", true);
-			capabilities.setBrowserName("firefox");
-			capabilities.setVersion("");
-			capabilities.setPlatform(Platform.ANY);
-			// WebDriver driver = new MarionetteDriver(capabilities); 
-
+			FirefoxFeatureProfile bPro = getBrowserProfile();			
+            setFirefoxDriverSystemEnv(); 
+            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+    		capabilities.setCapability(MARIONETTENAME, true);
+    		capabilities.setBrowserName(BROWSERTYPENAME);
+    		capabilities.setVersion("");
+    		capabilities.setPlatform(Platform.ANY);
 			if (null == bPro) {
 				retVal = new FirefoxDriver(capabilities);
 			} else {
